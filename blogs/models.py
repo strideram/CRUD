d5725d3cb11150607dfc0e2485e0021ec21b39fb
strideram <ramseskantolines@gmail.com>
2024-03-16 18:04:11 -0400

    create v2

--- blogs/models.py
+++ blogs/models.py
@@ -14,5 +14,5 @@ class Publication(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("publication_details", kwargs={"pk":self.pk})
+        return reverse("post_detail", kwargs={"pk":self.pk})
     
